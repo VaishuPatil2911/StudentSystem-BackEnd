74932ed2c558810a7a46553de16a5f03f06a3cdb
VaishuPatil2911 <vaishu291102@gmail.com>
2024-11-22 19:47:38 +0530

    Initial Commit

---
+++ src/main/java/com/codeavengers/studentsystem/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.codeavengers.studentsystem.repository;
+
+import com.codeavengers.studentsystem.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
